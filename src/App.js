ad63af2a84de9bd94f9424eeec585f47f62cc88b
mohtoto <totonchym@gmail.com>
2022-07-21 23:16:07 +0800

    new

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import Nav from "./components/nav";
+import Home from "./pages/home";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Books from "./pages/books";
+import { books } from './data'
+import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
+import React , {useState , useEffect} from "react";
+
+
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+
+  function addtoCart(book){
+
+  }
+  
+  useEffect(() => {
+    
+  }, [cart]);
+
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/books'  element={<Books books={books} />} />
+          <Route path='/books/:id' element={<BookInfo books={books} addtoCart={addtoCart}/>} /> 
+          <Route path='/cart/' element={<Cart books={books}/>} /> 
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars , faShoppingCart ,faTimes , faBolt , faBookOpen , faTags , faStar,faStarHalfAlt , faArrowLeft} from '@fortawesome/free-solid-svg-icons';
+
+
+
+library.add(faBars , faShoppingCart ,faTimes, faBolt , faBookOpen , faTags ,faStar,faStarHalfAlt , faArrowLeft)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( 
   <React.StrictMode>
     <App />
   </React.StrictMode>
